865c0566770a40ef614ba738a844bc760ac59bbf
Jan Salawa <salawa.jan@gmail.com>
2023-09-22 09:53:23 +0200

    updates

--- Frontend.Mvc/Program.cs
+++ Frontend.Mvc/Program.cs
@@ -10,42 +10,6 @@ builder.Services.AddHealthChecks().AddCheck("Health", () => HealthCheckResult.He
 
 var app = builder.Build();
 
-string basePath = app.Configuration["API_PATH_BASE"];
-/*
-app.Use((context, next) =>
-{
-    string _pathBase = basePath;
-    PathString matchedPath;
-    PathString remainingPath;
-
-    if (context.Request.Path.StartsWithSegments(_pathBase, out matchedPath, out remainingPath))
-    {
-        var originalPath = context.Request.Path;
-        var originalPathBase = context.Request.PathBase;
-        context.Request.Path = remainingPath;
-        context.Request.PathBase = originalPathBase.Add(matchedPath);
-        var re = context.Request.PathBase;
-        return next.Invoke();
-
-    }
-    else
-    {
-        return next.Invoke();
-    }
-});
-*/
-/*
-if (!string.IsNullOrWhiteSpace(basePath))
-{
-    app.UsePathBase($"/{basePath.TrimStart('/')}");
-}
-else
-{
-    app.UsePathBase("/frontend2");
-}
-*/
-app.UsePathBase("/frontend2");
-
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
